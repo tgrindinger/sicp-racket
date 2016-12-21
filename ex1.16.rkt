0a85991df19808812f97b331490cc546c3a3aac8
Thomas Grindinger <tgrindinger@gmail.com>
2016-12-20 22:31:25 -0600

    add ex1.16

---
+++ ex1.16.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(define (fast-expt b n)
+  (fast-expt-iter b (- n 1) b)
+)
+
+(define (fast-expt-iter b counter product)
+  (cond ((= counter 0)    product)
+        ((even? counter) (fast-expt-iter (* b b) (/ counter 2) product))
+        (else            (fast-expt-iter b (- counter 1) (* b product)))
+  )
+)
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (read-loop continue)
+  (if (string-prefix? (string-downcase (symbol->string continue)) "y")
+    (begin
+      (printf "Give me a number to exponentiate, now! ")
+      (let ([b (read)])
+        (printf "Now give me an exponent! ")
+        (let ([n (read)])
+          (printf "Raising ~a to the power of ~a with gives ~a!\n" b n (fast-expt b n))
+        )
+      )
+      (printf "Can you handle another? ")
+      (read-loop (read))
+    )
+    "That's it, now go away!"
+  )
+)
+
+(printf "Welcome to my blazingly fast exponentiator!\n")
+(printf "~a\n" (read-loop 'y))
+
